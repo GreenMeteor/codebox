ee9888ab2433959adc69a7f5947d2f43233ff894
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-12-11 18:42:05 -0500

    Enh: Implement AdminMenu permission restriction

--- Events.php
+++ Events.php
@@ -6,21 +6,30 @@ use Yii;
 use yii\helpers\Url;
 use yii\base\BaseObject;
 use humhub\models\Setting;
+use humhub\modules\ui\menu\MenuLink;
 use humhub\modules\ui\icon\widgets\Icon;
+use humhub\modules\admin\widgets\AdminMenu;
+use humhub\modules\admin\permissions\ManageModules;
 
 class Events extends BaseObject
 {
 
     public static function onAdminMenuInit($event)
     {
-        $event->sender->addItem([
+        if (!Yii::$app->user->can(ManageModules::class)) {
+            return;
+        }
+
+        /** @var AdminMenu $menu */
+        $menu = $event->sender;
+
+        $menu->addEntry(new MenuLink([
             'label' => Yii::t('CodeboxModule.base', 'Codebox Settings'),
             'url' => Url::toRoute('/codebox/admin/index'),
-            'group' => 'settings',
             'icon' => Icon::get('code'),
             'isActive' => Yii::$app->controller->module && Yii::$app->controller->module->id == 'codebox' && Yii::$app->controller->id == 'admin',
             'sortOrder' => 600,
-        ]);
+        ]));
     }
 
     public static function addCodeboxFrame($event)
