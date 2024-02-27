e166a657fba76c71d8d75fd8d6f0453ab0746fd2
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-02-27 02:03:33 -0500

    Enh: Encode Title
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- widgets/views/codeboxframe.php
+++ widgets/views/codeboxframe.php
@@ -8,7 +8,7 @@ use humhub\widgets\PanelMenu;
 <div class="panel panel-default panel-codebox" id="panel-codebox">
     <?= PanelMenu::widget(['id' => 'panel-codebox']); ?>
   <div class="panel-heading">
-    <p><?= $title ?></p>
+    <strong><?= Html::encode($title) ?></strong>
   </div>
   <div class="panel-body">
 
