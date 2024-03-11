972c1979a5cc4d889e3ac3381c4c1c02147568dd
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-03-11 07:10:17 -0400

    Update codeboxframe.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- widgets/views/codeboxframe.php
+++ widgets/views/codeboxframe.php
@@ -7,14 +7,10 @@ use humhub\widgets\PanelMenu;
 
 <div class="panel panel-default panel-codebox" id="panel-codebox">
     <?= PanelMenu::widget(['id' => 'panel-codebox']); ?>
-  <div class="panel-heading">
-    <strong><?= Html::encode($title) ?></strong>
-  </div>
-  <div class="panel-body">
-
-<?= Html::beginTag('div') ?>
-<?= $htmlCode ?>
-<?= Html::endTag('div'); ?>
-
-</div>
+    <div class="panel-heading">
+        <strong><?= Html::encode($title) ?></strong>
+    </div>
+    <div class="panel-body">
+        <?= $htmlCode ?>
+    </div>
 </div>
